653f5101d1ea7b14706d8ff46cd342ee1d6bf4c0
Kai <79684872+Ong-Yi-Kai@users.noreply.github.com>
2022-01-04 09:33:30 -0800

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 
+var calc = function() {
+  var bill = Number(document.getElementById('amount').value);
+  var split = Number(document.getElementById('NumPeople').value);
+  var percentage = Number(document.getElementById('percentage').value);
+  var tip = bill*(percentage/100);
+  var totalBill = tip+bill;
+  var splitPerPerson = totalBill/split;
+  document.getElementById('tip').innerHTML = '$' + Number(tip).toFixed(2);
+  document.getElementById('total').innerHTML = '$' + Number(totalBill).toFixed(2);
+  document.getElementById('splitbill').innerHTML = '$' + Number(splitPerPerson).toFixed(2);
+}
